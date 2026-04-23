bc69be728af91dd958d666b713d56f13c8cd0a28
Steel Legionnaire <162994122+Steel-Legionaire@users.noreply.github.com>
2026-04-23 10:30:12 -0400

    Wet sand will propogate out

--- src/particles/particles.js
+++ src/particles/particles.js
@@ -274,6 +274,22 @@ export class Sand extends MoveableSolid {
         this.setColor(this.colors);
         this.rect.position.set(this.x * this.tileSize, this.y * this.tileSize);
         this.addToStage(this.rect);
+
+        this.triggers = ['Water']
+    }
+
+    action(){
+        this.radiateHeat();
+
+        const neighbors = this.getNeighbors();
+
+        for( let n of neighbors){
+            if(n.particle && this.triggers.includes(`${n.particle.constructor.name}`)){
+                this.matrix.deleteParticle(n.x, n.y);
+                this.matrix.replaceParticle(this.x, this.y, WetSand);
+                return;
+            }
+        }
     }
 }
 
@@ -298,6 +314,10 @@ export class Ash extends MoveableSolid {
         this.setColor(this.colors);
         this.rect.position.set(this.x * this.tileSize, this.y * this.tileSize);
         this.addToStage(this.rect);
+    }
+
+    action(){
+
     }
 }
 
@@ -366,7 +386,7 @@ export class Charcoal extends MoveableSolid {
                 }else{
                     if(chance <= 60){
                         chance = Math.random() * 100;
-                        if (chance <= 50) {
+                        if (chance <= 20) {
                             this.matrix.replaceParticle(this.x, this.y, Ash);
                             return;
                         } else {
@@ -410,11 +430,15 @@ export class WetSand extends StiffSolid {
     constructor(x, y, app, matrix){
         super(x, y, false, true, 5, 0, app, matrix)
 
-        this.colors = [0xB8B17F, 0xA9A274, 0xC2BB88, 0x9F996B, 0xD0C98F];
+        this.colors = [0x000000];
 
         this.setColor(this.colors);
         this.rect.position.set(this.x * this.tileSize, this.y * this.tileSize);
         this.addToStage(this.rect);
+
+        this.lastTransferAttempt = 0;
+
+        this.triggers = ['Sand'];
     }
 
     action(){
@@ -424,6 +448,26 @@ export class WetSand extends StiffSolid {
             if(Math.random() * 100 < (10 + this.temp)){
                 this.matrix.replaceParticle(this.x, this.y, Sand);
             }
+        }
+
+        const now = performance.now();
+
+        if (now - this.lastTransferAttempt >= 5000) {
+            const neighbors = this.getNeighbors();
+
+            for( let n of neighbors){
+                if(n.particle && this.triggers.includes(`${n.particle.constructor.name}`)){
+                    if(n.y >= this.y && n.particle instanceof Sand){
+                        let chance = Math.random() * 100;
+                        if(chance <= 20){
+                            this.matrix.replaceParticle(n.x, n.y, WetSand);
+                            this.matrix.replaceParticle(this.x, this.y, Sand);
+                            return;
+                        }
+                    }
+                }
+            }
+            this.lastTransferAttempt = now;
         }
     }
 }
